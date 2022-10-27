1af9328def4ce2b61668c0961943471967978387
min <smh92333@gmail.com>
2022-10-27 17:40:09 +0900

    login, signup 기능구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, redirect
-# from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
 
 def login(request):
     
@@ -9,10 +22,12 @@ def login(request):
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('articles:cards')
+            return redirect('articles:index')
     else:
         form = AuthenticationForm()
     context = {
         'form': form
     }
     return render(request, 'accounts/login.html')
+
+    
