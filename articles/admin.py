0dccb667058f4a4967df61f6d503f11175f07ae8
jejoonlee <jejoonlee1996@gmail.com>
2022-10-27 15:01:17 +0900

    articles 인덱스 페이지, 리뷰 페이지 기초

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Teacher, Comment
 
 # Register your models here.
+class TeacherAdmin(admin.ModelAdmin):
+  pass
+
+class CommentAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Comment, CommentAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+  name = models.CharField(max_length=200)
+  english_name = models.CharField(max_length=200)
+  image = models.ImageField(upload_to='image/teacher_image', blank=True)
+
+class Comment(models.Model):
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
